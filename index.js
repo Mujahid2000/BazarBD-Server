75d536317a7484dc741f2a69b52db92ac132fa44
Mujahid2000 <your.email@example.com>
2024-01-14 01:55:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5050;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mujahid.frqpuda.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const ProductCollection = client.db("BazarBD").collection("Product-Collection");
+
+    app.get('/addProduct', async (req, res) =>{
+        const result = await ProductCollection.find().toArray();
+        res.send(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  } 
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res) =>{
+    res.send('Bazar running server')
+})
+
+app.listen(port, () =>{
+    console.log(`server is running on port ${port}`);
+})
